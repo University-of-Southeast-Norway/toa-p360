173c741b847ae54608d438f57c06bba43a9b3abf
Øivind Skovborg <oskov@usn.no>
2024-02-15 17:14:17 +0100

    Refactor: Removed logic not in use

--- ToaArchiver.Domain/Messages/ContractSignedMessage.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace ToaArchiver.Domain.Messages;
-
-public record class ContractSignedMessage(string SequenceNumber, string EmployeeId, string Id = default!, string CorrelationId = default!, string Source = default!, string Subject = default!, string Uri = default!, DateTimeOffset ValidAfter = default)
-    : MessageBase(Id, CorrelationId, Source, Subject, Uri, ValidAfter);
